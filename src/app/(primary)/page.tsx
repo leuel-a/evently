7049982e472cb1fe4993b8f420875aa40e6c082c
leuel-a <leuela1993@gmail.com>
2024-10-10 14:48:10 +0300

    feat: fix the auth page layout

--- src/app/(primary)/page.tsx
+++ src/app/(primary)/page.tsx
@@ -1,10 +1,19 @@
+import { ChevronRight } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+
 export default function Page() {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-full py-40 text-center bg-indigo-600 text-white">
+      <div className="col-span-full w-full bg-gradient-to-r from-indigo-700 to-indigo-400 pb-10 pt-20 text-center text-white">
         <h1 className="text-7xl">
-          Discover Your Next Great <span className="text-black italic">Event!</span>
+          Discover Your Next Great{' '}
+          <span className="font-medium italic text-yellow-500">Event!</span>
         </h1>
+        <p className="text-2xl">Explore tech events, seminars, and more in your area</p>
+        <div className="mt-8 space-x-3 font-semibold">
+          <Button>Explore Events</Button>
+          <Button>Are you are Organizer?</Button>
+        </div>
       </div>
     </div>
   )
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -19,60 +19,62 @@ export default function Header() {
   const user = session?.data?.user
 
   return (
-    <header className="min-w-container mx-auto flex max-w-container items-center justify-between px-4 py-4 xl:px-0">
-      <Link href="/" className="select-none text-3xl font-semibold">
-        evently
-      </Link>
+    <header className="bg-indigo-700 text-white shadow-lg border-indigo-800">
+      <div className="min-w-container mx-auto flex max-w-container items-center justify-between px-4 py-4 xl:px-0">
+        <Link href="/" className="select-none text-3xl font-semibold">
+          evently
+        </Link>
 
-      {expanded && (
-        <div
+        {expanded && (
+          <div
+            onClick={() => setExpanded((prev) => !prev)}
+            className="fixed bottom-0 left-0 right-0 top-0 z-10 md:hidden"
+          ></div>
+        )}
+
+        <button
           onClick={() => setExpanded((prev) => !prev)}
-          className="fixed bottom-0 left-0 right-0 top-0 z-10 md:hidden"
-        ></div>
-      )}
-
-      <button
-        onClick={() => setExpanded((prev) => !prev)}
-        className={cn(
-          'absolute right-4 top-4 z-50 aspect-square w-8 bg-cover bg-center bg-no-repeat md:hidden',
-        )}
-      >
-        {expanded ? (
-          <X size={24} className="aspect-square text-black" />
-        ) : (
-          <Menu size={24} className="aspect-square text-black" />
-        )}
-      </button>
-
-      <nav className="font-medium">
-        <ul
           className={cn(
-            'fixed bottom-0 text-white md:text-black left-1/3 right-0 top-0 z-20 flex flex-col gap-20 px-8 py-28 transition-transform duration-500 ease-out md:static md:translate-x-0 md:flex-row md:items-center md:justify-between md:py-0',
-            expanded
-              ? 'translate-x-0 bg-indigo-500 bg-opacity-50 backdrop-blur'
-              : 'translate-x-full',
+            'absolute right-4 top-4 z-50 aspect-square w-8 bg-cover bg-center bg-no-repeat md:hidden',
           )}
         >
-          <div onClick={() => setExpanded(false)} className="flex flex-col gap-6 md:flex-row">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/events">Explore Events</Link>
-            </li>
-            <li>
-              <Link href="/events">Categories</Link>
-            </li>
-          </div>
-        </ul>
-      </nav>
-      {session.status === 'loading' ? (
-        <p>Loading...</p>
-      ) : session.data ? (
-        <UserMenu user={user} />
-      ) : (
-        <SignInButton setExpanded={setExpanded} />
-      )}
+          {expanded ? (
+            <X size={24} className="aspect-square text-black" />
+          ) : (
+            <Menu size={24} className="aspect-square text-black" />
+          )}
+        </button>
+
+        <nav className="font-medium">
+          <ul
+            className={cn(
+              'fixed bottom-0 left-1/3 right-0 top-0 z-20 flex flex-col gap-20 px-8 py-28 text-white transition-transform duration-500 ease-out md:static md:translate-x-0 md:flex-row md:items-center md:justify-between md:py-0 md:text-black',
+              expanded
+                ? 'translate-x-0 bg-indigo-500 bg-opacity-50 backdrop-blur'
+                : 'translate-x-full',
+            )}
+          >
+            <div onClick={() => setExpanded(false)} className="flex text-white flex-col gap-6 md:flex-row">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/events">Explore Events</Link>
+              </li>
+              <li>
+                <Link href="/events">Categories</Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+        {session.status === 'loading' ? (
+          <p>Loading...</p>
+        ) : session.data ? (
+          <UserMenu user={user} />
+        ) : (
+          <SignInButton setExpanded={setExpanded} />
+        )}
+      </div>
     </header>
   )
 }
