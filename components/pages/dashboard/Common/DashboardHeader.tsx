5d9ffcb33ad66c4ac8ff1d531137a547cf7ed786
leuel <leuela1993@gmail.com>
2025-07-16 13:10:41 +0300

    fix: don't show the create button on create routes only on list pages

--- components/pages/dashboard/Common/DashboardHeader.tsx
+++ components/pages/dashboard/Common/DashboardHeader.tsx
@@ -1,12 +1,15 @@
+import {usePathname} from 'next/navigation';
 import {useResourcesContext} from '@/context/ResourcesContext';
 import {useResourceContext} from '@/context/ResourceContext';
 import {CreateResourceButton} from './CreateResourceButton';
 import {DashboardBreadcrumb} from './DashboardBreadcrumb';
 
 export function DashboardHeader(_: DashboardHeaderProps) {
+    const pathname = usePathname();
     const resources = useResourcesContext();
     const resource = useResourceContext();
-    const shouldShowCreateButton = resources.find((res) => res.name === resource)?.hasCreate ?? false;
+    const isCreatePath = pathname.split('/').includes('create');
+    const shouldShowCreateButton = (resources.find((res) => res.name === resource)?.hasCreate && !isCreatePath) ?? false;
 
     return (
         <div className="flex h-20 w-full items-center justify-between px-2">
