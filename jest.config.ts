f91174cd8790b02844a3c5640bc7ab9314dc3e37
leuel <leuela1993@gmail.com>
2025-08-04 13:01:40 +0300

    add configuration for testing with jest and react testing library

---
+++ jest.config.ts
@@ -0,0 +1,23 @@
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+    dir: './',
+});
+
+const customConfiguration: Config = {
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default async () => {
+    const config = await createJestConfig(customConfiguration)();
+
+    return {
+        ...config,
+        transformIgnorePatterns: [
+            '/node_modules/(?!(nanostores)/)'
+        ]
+    };
+}
