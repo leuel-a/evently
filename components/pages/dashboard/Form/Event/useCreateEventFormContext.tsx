7329b14bfe8af64158fbd2431a919e32448a2998
leuel <leuela1993@gmail.com>
2025-08-04 23:28:44 +0300

    feat: add lint-staged with husky for pre-commit hooks

--- components/pages/dashboard/Form/Event/useCreateEventFormContext.tsx
+++ components/pages/dashboard/Form/Event/useCreateEventFormContext.tsx
@@ -1,5 +1,5 @@
 import {useFormContext} from 'react-hook-form';
-import type {EventSchemaType} from './schema';
+import type {EventSchemaType} from '@/lib/db/schema';
 
 export const useCreateEventFormContext = () => {
     return useFormContext<EventSchemaType>();
