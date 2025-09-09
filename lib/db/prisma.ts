0bf99a4e66cf0b72f69b7fdf517d11869958f4a8
leuel <leuela1993@gmail.com>
2025-09-09 15:56:59 +0300

    feat: ditch accelerate for prisma

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,11 +1,10 @@
-import {withAccelerate} from '@prisma/extension-accelerate';
 import {PrismaClient} from '@/app/generated';
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
